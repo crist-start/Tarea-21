372457c0156385557f621831ae403d15db4bdb68
Cristopher Erick Utrilla Tranquilino <31595668+crist-start@users.noreply.github.com>
2017-11-10 01:02:50 -0600

    Add files via upload

---
+++ letras.h
@@ -0,0 +1,86 @@
+/*funciones de tipo vacio (void) que imprimen las letras del nombre "cristopher",
+Estas letras se imprimiran con un conjuto de su letra correspondiente
+ con un alto de 5 por un ancho de 4 unidades*/
+
+/*funciones de letras*/
+
+//letra C
+void letraC(){
+	printf("CCCC\n");
+	printf("C   \n");
+	printf("C   \n");
+	printf("C   \n");
+	printf("CCCC\n");
+}
+
+//letra E
+void letraE(){
+	printf("EEEE\n");
+	printf("E   \n");
+	printf("EEE \n");
+	printf("E   \n");
+	printf("EEEE\n");
+}
+
+//letra H
+void letraH(){
+	printf("H  H\n");
+	printf("H  H\n");
+	printf("HHHH\n");
+	printf("H  H\n");
+	printf("H  H\n");
+}
+
+//letra I
+void letraI(){
+	printf("IIII\n");
+	printf(" II \n");
+	printf(" II \n");
+	printf(" II \n");
+	printf("IIII\n");
+}
+
+//letra O
+void letraO(){
+	printf("OOOO\n");
+	printf("O  O\n");
+	printf("O  O\n");
+	printf("O  O\n");
+	printf("OOOO\n");
+}
+
+//letra P
+void letraP(){
+	printf("PPPP\n");
+	printf("P  P\n");
+	printf("PPPP\n");
+	printf("P   \n");
+	printf("P   \n");
+}
+
+//letra R
+void letraR(){
+	printf("RRR \n");
+	printf("R  R\n");
+	printf("RRR  \n");
+	printf("R R \n");
+	printf("R  R\n");
+}
+
+//letra S
+void letraS(){
+	printf("SSSS\n");
+	printf("S   \n");
+	printf("SSSS\n");
+	printf("   S\n");
+	printf("SSSS\n");
+}
+
+//letra T
+void letraT(){
+	printf("TTTT\n");
+	printf(" TT \n");
+	printf(" TT \n");
+	printf(" TT \n");
+	printf(" TT \n");
+}
---
+++ nombre.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "letras.h"//biblioteca creada por el programador
+
+int main(){
+	printf("Impresion del nombre cristopher en pantalla de forma grande\n");
+	//uso de las funciones de la biblioteca letras.h para imprimir
+	letraC();
+	printf("\n");
+	letraR();
+	printf("\n");
+	letraI();
+	printf("\n");
+	letraS();
+	printf("\n");
+	letraT();
+	printf("\n");
+	letraO();
+	printf("\n");
+	letraP();
+	printf("\n");
+	letraH();
+	printf("\n");
+	letraE();
+	printf("\n");
+	letraR();
+	
+	return 0;
+}
